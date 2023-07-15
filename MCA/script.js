141382a1fb13c1bb10346b1c0fb3663a45ba6970
Vinayak Singh <111453157+singhvinayak71@users.noreply.github.com>
2023-07-15 23:16:22 +0530

    second commit
    adding script.js

---
+++ MCA/script.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("btn");
+
+btn.addEventListener('click',()=>{
+    const name = document.getElementById('name');
+    const mobile = document.getElementById('mobile').value;
+    console.log(mobile*4)
+    console.log("hello this is me")
+})
